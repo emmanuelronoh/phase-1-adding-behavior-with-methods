1a42f9765ea72e81e7ea9c43ad9ccee0f1607b62
emmanuel ronoh <eronoh036@gmail.com>
2024-07-17 11:12:49 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+// Cat class 
+class Cat {
+    constructor(name, gender) {
+      this.name = name || "Unknown";
+      this.gender = gender || "Unknown";
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  // Dog class 
+  class Dog {
+    constructor(name, gender) {
+      this.name = name || "Unknown";
+      this.gender = gender || "Unknown";
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  //bird class
+  class Bird {
+    constructor(name, gender) {
+      this.name = name || "Unknown";
+      this.gender = gender || "Unknown";
+    }
+  
+    speak() {
+      if (this.gender === "male") {
+        return `It's me! ${this.name}, the parrot!`;
+      } else if (this.gender === "female") {
+        return `${this.name} says squawk!`;
+      } else {
+        return "Unknown bird says squawk!";
+      }
+    }
+  }
+  
+ //exporting classes
+  module.exports = {
+    Cat,
+    Dog,
+    Bird
+  };
+  
